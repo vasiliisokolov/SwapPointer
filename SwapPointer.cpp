60828baf9f4882300f6c5abb42eedbd5d7fee769
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-20 21:20:07 +0300

    Добавьте файлы проекта.

---
+++ SwapPointer.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void swap_pointer(int&, int&);
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    std::cout << "A and B before swap: A-" << a << " B-" << b << std::endl;
+    swap_pointer(a, b);
+    std::cout << "A and B after swap: A-" << a << " B-" << b << std::endl;
+}
+
+void swap_pointer(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
